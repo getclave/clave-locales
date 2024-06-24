15aec91b69678418163e885f6821266f6f51d1ec
asgarovf <fasgarov2002@gmail.com>
2024-06-24 17:59:51 +0400

    [clave-locales] fix: copyright

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright Clave - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
 import { type en } from './en';
 
 export type Locale = typeof en;
