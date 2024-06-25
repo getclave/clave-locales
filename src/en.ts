425414aabef26ec32349517ec03be4761ae1ab70
asgarovf <fasgarov2002@gmail.com>
2024-06-25 10:41:38 +0400

    [clave-locales] fix: circlular dependency

--- src/en.ts
+++ src/en.ts
@@ -3,9 +3,8 @@
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  */
-import { type Locale } from './types';
 
-export const en: Locale = {
+export const en = {
     locale: 'en',
     general: {
         identifiers: {
