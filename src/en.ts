b627d00077267c184304c7ad3f7a4a71a59bb732
asgarovf <fasgarov2002@gmail.com>
2024-09-19 13:44:43 +0300

    [zksync-mobile] feat: show contact names on recently sent

--- src/en.ts
+++ src/en.ts
@@ -1952,7 +1952,7 @@ export const en = {
             'Add your friend to your address book to easily send them money in the future.',
         label: 'Custom Name',
         button: 'Add to Friends',
-        button_edit: 'Edit',
+        button_edit: 'Save',
     },
     user_profile: {
         title: 'Profile',
